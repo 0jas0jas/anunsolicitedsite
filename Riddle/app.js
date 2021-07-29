b6e10396b4e217b59712bfaa935c09e7ee8f2b04
Ojas Srivastava <ojas1807@gmail.com>
2021-07-29 20:19:51 +0530

    riddlesssssss

---
+++ Riddle/app.js
@@ -0,0 +1,9 @@
+function submit() {
+    const riddle1 = document.querySelector('#riddle1').value;
+    const riddle2 = document.querySelector('#riddle2').value;
+    const riddle3 = document.querySelector('#riddle3').value;
+
+    console.log(riddle1);
+    console.log(riddle2);
+    console.log(riddle3);
+}
